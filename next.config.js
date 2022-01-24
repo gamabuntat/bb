3a5d479f6d54fe110f9e17ee1c06f28162bc13eb
gamabuntat <schoshoev@yandex.ru>
2022-01-24 22:19:40 +0800

    env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    const alias = {
+      ...config.resolve.alias,  
+      'styles': path.resolve(__dirname, 'src/styles'),
+      'components': path.resolve(__dirname, 'src/components'),
+    };
+
+    config.resolve.alias = alias;
+
+    return config;
+  },
+};
 
 module.exports = nextConfig
