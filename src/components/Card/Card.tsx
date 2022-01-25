e9f7a5dc0c8e5d718a35c4490b4d88315d3a866e
gamabuntat <schoshoev@yandex.ru>
2022-01-25 20:33:53 +0800

    added card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+
+import styles from './Card.module.sass';
+
+const Card = (
+  props: React.HTMLProps<HTMLButtonElement> &
+    React.PropsWithChildren<{ photo?: string }>
+) => {
+  const defaultProps = { ...props, children: null, photo: null };
+  const { photo = '/noImg.webp' } = props;
+
+  return (
+    <button
+      {...defaultProps}
+      className={styles.Card}
+      type="button"
+      aria-label="card"
+    >
+      <div className={styles.ImgContainer}>
+        <Image
+          src={photo}
+          alt={photo}
+          width="180px"
+          height="220px"
+          priority={false}
+          objectFit="cover"
+        />
+      </div>
+      <div className={styles.Details}>{props.children}</div>
+    </button>
+  );
+};
+export default Card;
