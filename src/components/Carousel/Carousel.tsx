c9cfdebee0eab7b951b1cd5375a7e339981b3cc7
gamabuntat <schoshoev@yandex.ru>
2022-01-25 20:34:11 +0800

    added carousel

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,98 @@
+import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
+
+import { Episode } from 'src/api/types';
+import Card from 'src/components/Card/Card';
+
+import styles from './Carousel.module.sass';
+
+const Carousel = ({ episodes = [] }: { episodes: Episode[] }): JSX.Element => {
+  const router = useRouter();
+  const [isBinded, toggleBinded] = useState(false);
+  const [isScroll, toggleIsScroll] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [position, setPosition] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  const [carouselSize, setCarouselSize] = useState(0);
+  const [containerSize, setContainerSize] = useState(0);
+  const calcMax = () => carouselSize - containerSize;
+
+  const handleResizeWindow = () => {
+    setCarouselSize(carousel.current?.getBoundingClientRect().width ?? 0);
+    setContainerSize(container.current?.getBoundingClientRect().width ?? 0);
+  };
+
+  const updateMax = () => {
+    handleResizeWindow();
+    return calcMax();
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResizeWindow);
+
+    return () => {
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+  }, []);
+
+  const onPointerDown: React.PointerEventHandler<HTMLDivElement> = (e) => {
+    (e.target as HTMLDivElement).setPointerCapture(e.pointerId);
+    toggleBinded(true);
+    toggleIsScroll(false);
+    setOffset(e.clientX - position);
+  };
+
+  const onPointerMove: React.PointerEventHandler<HTMLDivElement> = (e) => {
+    if (isBinded) {
+      e.preventDefault();
+      toggleIsScroll(true);
+      setPosition(
+        Math.max(
+          Math.min(e.clientX - offset, 0),
+          calcMax() || updateMax() || -Infinity
+        )
+      );
+    }
+  };
+
+  return (
+    <div
+      ref={carousel}
+      className={styles.Carousel}
+      onPointerDown={onPointerDown}
+      onPointerMove={onPointerMove}
+      onLostPointerCapture={() => {
+        toggleBinded(false);
+      }}
+    >
+      <div
+        ref={container}
+        className={styles.Container}
+        style={{ left: `${position}px` }}
+      >
+        {episodes.map((episode) => (
+          <Card
+            key={episode.episode_id}
+            title={episode.title}
+            onClick={() => {
+              if (!isScroll) {
+                router.push(`/episode/${episode.episode_id}`);
+              }
+            }}
+          >
+            <div className={styles.Details}>
+              <h3 className={styles.DetailsTitle}>{episode.title}</h3>
+              <div className={styles.DetailsContainer}>
+                <span>#{episode.episode}</span>
+                {episode.air_date}
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
